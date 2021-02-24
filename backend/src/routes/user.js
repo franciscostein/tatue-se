ca659ef934d58222a911b7bf904d8fa4f941fc92
Leandro Costa <leandro.franc2008@hotmail.com>
2021-02-23 23:45:11 -0300

    user and artist routes

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 
 const router = new express.Router();
 
-// logging in
+// login
 router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password);
@@ -18,7 +18,57 @@ router.post('/users/login', async (req, res) => {
     }
 });
 
-// insert new user
+// logout
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter(toke => {
+            return token.token !== req.token;
+        });
+
+        await req.user.save();
+
+        res.send();
+    }
+    catch(e) {
+        res.status(500).send();
+    }
+});
+
+// logout all
+router.post('/users/logout-all', auth, async (req, res) => {
+    try {
+        req.user.tokens = [];
+
+        await req.user.save();
+
+        res.send();
+    }
+    catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+// get
+router.get('/users/:id', auth, async (req, res) => {
+    if (!req.user.type === 'ADMIN') {
+        res.send(req.user);
+    }
+
+    try {
+        const user = await User.findOne({ _id: req.params.id });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+        
+        res.send(user);
+    }
+    catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+// insert new
 router.post('/users', authAdmin, async (req, res) => {
     const user = new User(req.body);
 
@@ -30,6 +80,58 @@ router.post('/users', authAdmin, async (req, res) => {
     }
     catch(e) {
         res.status(400).send(e);
+    }
+});
+
+// update
+router.patch('/users/:id', auth, async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdatesUser = ['password'];
+    const allowedUpdatesAdmin = ['email', 'password', 'type'];
+    let isValidOperation = false;
+
+    if (req.user.type === 'ADMIN') {
+        isValidOperation = updates.every(update => allowedUpdatesAdmin.includes(update));
+    }
+    else {
+        isValidOperation = updates.every(update => allowedUpdatesUser.includes(update));
+    }
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Update denied' });
+    }
+
+    try {
+        const user = await User.findOne({ _id: req.params.id });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        updates.forEach(update => user[update] = req.body[update]);
+
+        await user.save();
+
+        res.send(user);
+    }
+    catch(e) {
+        res.status(500).send(e);
+    }
+});
+
+// delete
+router.delete('/users/:id', authAdmin, async (req, res) => {
+    try {
+        const user = await User.findOneAndDelete({ _id: req.params.id });
+
+        if (!user) {
+            return res.status(404).send();
+        }
+
+        res.send(user);
+    }
+    catch(e) {
+        res.status(500).send();
     }
 });
 
