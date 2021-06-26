d67d5cb7e3a7f11a363a5462ef2f67f1df50b055
Leandro Costa <leandro.franc2008@hotmail.com>
2021-06-26 10:37:25 -0300

    msw boilerplate

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,14 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+import { server } from './mocks/server';
+
+// establish API mocking before all tests
+beforeAll(() => server.listen());
+
+// reset any request handlers that may be added during tests, so they don't affect others
+afterEach(() => server.resetHandlers());
+
+// clean up after the tests are finished
+afterAll(() => server.close());
