956ca747388ad506aff4d9b3c3380add1804d42e
Leandro Costa <leandro.franc2008@hotmail.com>
2021-07-20 22:59:13 -0300

    artists inline styles cleared

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import Navbar from './layout/navbar/Navbar';
 import Footer from './layout/footer/Footer';
 // import Studios from './components/studios/Studios';
-// import Artists from './components/artists/Artists';
-import Artist from './components/artists/artist/Artist';
+import Artists from './components/artists/Artists';
+// import Artist from './components/artists/artist/Artist';
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <div className="App-content">
-                <Artist />
+                <Artists />
             </div>
             <Footer />
         </div>
--- frontend/src/components/artists/Artists.jsx
+++ frontend/src/components/artists/Artists.jsx
@@ -1,3 +1,4 @@
+import './Artists.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -13,7 +14,7 @@ const Artists = () => {
     return (
         <div>
             <h1 className="mt-5">Artists</h1>
-            <p style={{ fontSize: '70%', color: 'gray' }}>Find your next tattoo artist.</p>
+            <p className="font-70 secondary-color">Find your next tattoo artist.</p>
             <Row className="justify-content-md-center my-4">
                 <Col md={{ span: 4 }}>
                     <Form.Group controlId="formArtistLocation">
@@ -23,95 +24,95 @@ const Artists = () => {
             </Row>
             <hr className="mb-3" />
             <Row className="d-flex justify-content-center">
-                <Col style={{ borderRadius: '16px', backgroundColor: '#212529', flex: '0 0 400px' }} className="m-3">
+                <Col className="studio-card m-3">
                     <Row>
-                        <Image src={img1} style={{ height: '215px', padding: 0, borderTopLeftRadius: '16px', borderTopRightRadius: '16px', objectFit: 'cover' }} />
+                        <Image src={img1} className="studio-card-img" />
                     </Row>
                     <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} style={{ margin: '10px 25px 10px 0', height: '35px', width: '35px' }} roundedCircle />
+                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
                         <Col>
-                            <Row style={{ fontSize: '60%' }}>Tattoist 1</Row>
-                            <Row style={{ fontSize: '45%' }}>Tattoo Studio</Row>
+                            <Row className="font-60">Tattoist 1</Row>
+                            <Row className="font-45">Tattoo Studio</Row>
                         </Col>
                     </div>
-                    <div style={{ borderTop: '1px dashed #ffffff30' }} className="d-flex flex-wrap pt-2 pb-1">
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Blackwork</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Dotwork</span>
+                    <div className="dashed-top-border-secondary d-flex flex-wrap pt-2 pb-1">
+                        <span className="tattoo-style-badge mx-1">Blackwork</span>
+                        <span className="tattoo-style-badge mx-1">Dotwork</span>
                     </div>
                 </Col>
-                <Col style={{ borderRadius: '16px', backgroundColor: '#212529', flex: '0 0 400px' }} className="m-3">
+                <Col className="studio-card m-3">
                     <Row>
-                        <Image src={img2} style={{ height: '215px', padding: 0, borderTopLeftRadius: '16px', borderTopRightRadius: '16px', objectFit: 'cover' }} />
+                        <Image src={img2} className="studio-card-img" />
                     </Row>
                     <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} style={{ margin: '10px 25px 10px 0', height: '35px', width: '35px' }} roundedCircle />
+                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
                         <Col>
-                            <Row style={{ fontSize: '60%' }}>Tattoist 2</Row>
-                            <Row style={{ fontSize: '45%' }}>Tattoo Studio</Row>
+                            <Row className="font-60">Tattoist 2</Row>
+                            <Row className="font-45">Tattoo Studio</Row>
                         </Col>
                     </div>
-                    <div style={{ borderTop: '1px dashed #ffffff30' }} className="d-flex flex-wrap pt-2 pb-1">
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Blackwork</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Neo-Tradicional</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Realism</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Illustrative</span>
+                    <div className="dashed-top-border-secondary d-flex flex-wrap pt-2 pb-1">
+                        <span className="tattoo-style-badge mx-1">Blackwork</span>
+                        <span className="tattoo-style-badge mx-1">Neo-Tradicional</span>
+                        <span className="tattoo-style-badge mx-1">Realism</span>
+                        <span className="tattoo-style-badge mx-1">Illustrative</span>
                     </div>
                 </Col>
-                <Col style={{ borderRadius: '16px', backgroundColor: '#212529', flex: '0 0 400px' }} className="m-3">
+                <Col className="studio-card m-3">
                     <Row>
-                        <Image src={img3} style={{ height: '215px', padding: 0, borderTopLeftRadius: '16px', borderTopRightRadius: '16px', objectFit: 'cover' }} />
+                        <Image src={img3} className="studio-card-img" />
                     </Row>
                     <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} style={{ margin: '10px 25px 10px 0', height: '35px', width: '35px' }} roundedCircle />
+                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
                         <Col>
-                            <Row style={{ fontSize: '60%' }}>Tattoist 3</Row>
-                            <Row style={{ fontSize: '45%' }}>Tattoo Studio</Row>
+                            <Row className="font-60">Tattoist 3</Row>
+                            <Row className="font-45">Tattoo Studio</Row>
                         </Col>
                     </div>
-                    <div style={{ borderTop: '1px dashed #ffffff30' }} className="d-flex flex-wrap pt-2 pb-1">
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Black &amp; Gray</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Blackwork</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Dotwork</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Fineline</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Ornamental</span>
+                    <div className="dashed-top-border-secondary d-flex flex-wrap pt-2 pb-1">
+                        <span className="tattoo-style-badge mx-1">Black &amp; Gray</span>
+                        <span className="tattoo-style-badge mx-1">Blackwork</span>
+                        <span className="tattoo-style-badge mx-1">Dotwork</span>
+                        <span className="tattoo-style-badge mx-1">Fineline</span>
+                        <span className="tattoo-style-badge mx-1">Ornamental</span>
                     </div>
                 </Col>
-                <Col style={{ borderRadius: '16px', backgroundColor: '#212529', flex: '0 0 400px' }} className="m-3">
+                <Col className="studio-card m-3">
                     <Row>
-                        <Image src={img4} style={{ height: '215px', padding: 0, borderTopLeftRadius: '16px', borderTopRightRadius: '16px', objectFit: 'cover' }} />
+                        <Image src={img4} className="studio-card-img" />
                     </Row>
                     <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} style={{ margin: '10px 25px 10px 0', height: '35px', width: '35px' }} roundedCircle />
+                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
                         <Col>
-                            <Row style={{ fontSize: '60%' }}>Tattoist 4</Row>
-                            <Row style={{ fontSize: '45%' }}>Tattoo Studio</Row>
+                            <Row className="font-60">Tattoist 4</Row>
+                            <Row className="font-45">Tattoo Studio</Row>
                         </Col>
                     </div>
-                    <div style={{ borderTop: '1px dashed #ffffff30' }} className="d-flex flex-wrap pt-2 pb-1">
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Blackwork</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Fineline</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Neo-Tradition</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Illustrative</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Surrealism</span>
+                    <div className="dashed-top-border-secondary d-flex flex-wrap pt-2 pb-1">
+                        <span className="tattoo-style-badge mx-1">Blackwork</span>
+                        <span className="tattoo-style-badge mx-1">Fineline</span>
+                        <span className="tattoo-style-badge mx-1">Neo-Tradition</span>
+                        <span className="tattoo-style-badge mx-1">Illustrative</span>
+                        <span className="tattoo-style-badge mx-1">Surrealism</span>
                     </div>
                 </Col>
-                <Col style={{ borderRadius: '16px', backgroundColor: '#212529', flex: '0 0 400px' }} className="m-3">
+                <Col className="studio-card m-3">
                     <Row>
-                        <Image src={img5} style={{ height: '215px', padding: 0, borderTopLeftRadius: '16px', borderTopRightRadius: '16px', objectFit: 'cover' }} />
+                        <Image src={img5} className="studio-card-img" />
                     </Row>
                     <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} style={{ margin: '10px 25px 10px 0', height: '35px', width: '35px' }} roundedCircle />
+                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
                         <Col>
-                            <Row style={{ fontSize: '60%' }}>Tattoist 5</Row>
-                            <Row style={{ fontSize: '45%' }}>Tattoo Studio</Row>
+                            <Row className="font-60">Tattoist 5</Row>
+                            <Row className="font-45">Tattoo Studio</Row>
                         </Col>
                     </div>
-                    <div style={{ borderTop: '1px dashed #ffffff30' }} className="d-flex flex-wrap pt-2 pb-1">
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Blackwork</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Fineline</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Neo-Tradition</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Illustrative</span>
-                        <span style={{ border: '1px solid #ffffff30', marginBottom: '3px', borderRadius: '16px', fontSize: '10px', padding: '7px', whiteSpace: 'nowrap' }} className="mx-1">Surrealism</span>
+                    <div className="dashed-top-border-secondary d-flex flex-wrap pt-2 pb-1">
+                        <span className="tattoo-style-badge mx-1">Blackwork</span>
+                        <span className="tattoo-style-badge mx-1">Fineline</span>
+                        <span className="tattoo-style-badge mx-1">Neo-Tradition</span>
+                        <span className="tattoo-style-badge mx-1">Illustrative</span>
+                        <span className="tattoo-style-badge mx-1">Surrealism</span>
                     </div>
                 </Col>
             </Row>
