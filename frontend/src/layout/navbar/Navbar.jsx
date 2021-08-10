9708e8e4e4a60d194e1e56bcd25c8fb8efd0e299
Leandro Costa <leandro.franc2008@hotmail.com>
2021-08-10 12:20:58 -0300

    profile dropdown fix

--- frontend/src/layout/navbar/Navbar.jsx
+++ frontend/src/layout/navbar/Navbar.jsx
@@ -6,6 +6,13 @@ import Image from 'react-bootstrap/Image';
 import { FaBars } from 'react-icons/fa';
 import userSolid from '../../assets/user_w.png';
 
+// Font awesome pixel sizes relative to the multiplier. 
+// 1x - 14px
+// 2x - 28px
+// 3x - 42px
+// 4x - 56px
+// 5x - 70px
+
 const NavbarComponent = () => {
     return (
         <Navbar bg="dark" expand="lg" sticky="top">
@@ -18,8 +25,8 @@ const NavbarComponent = () => {
 
             <Dropdown className="px-4">
                 <Dropdown.Toggle variant="dark" id="dropdown-profile">
-                    <Image src={userSolid} className="min-user px-2" roundedCircle />
-                    <FaBars />
+                    <Image src={userSolid} className="min-user px-3" roundedCircle />
+                    <FaBars size={28} />
                 </Dropdown.Toggle>
 
                 <Dropdown.Menu className="dropdown-menu">
