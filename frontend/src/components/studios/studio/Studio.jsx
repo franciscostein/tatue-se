47dbaa9d5125e5f82e6682d43629c71c18100f37
Leandro Costa <leandro.franc2008@hotmail.com>
2021-08-11 12:16:35 -0300

    adding map to studio layout

--- frontend/src/components/studios/studio/Studio.jsx
+++ frontend/src/components/studios/studio/Studio.jsx
@@ -1,10 +1,12 @@
 import './Studio.css';
 import Image from 'react-bootstrap/Image';
 import Row from 'react-bootstrap/Row';
-import { FaStar } from 'react-icons/fa';
+import Col from 'react-bootstrap/Col';
+import { FaStar, FaMapMarkerAlt } from 'react-icons/fa';
 
 import cover from '../../../assets/studio/1.jpeg';
 import profileImg from '../../../assets/user_w.png';
+import map from '../../../assets/staticmap.png';
 
 const Studios = () => {
     return (
@@ -40,19 +42,29 @@ const Studios = () => {
                     </div>
                 </div>
             </div>
-
-            <div className="mt-5 mx-5">
-                <h2 className="d-flex solid-bottom-border-secondary">About</h2>
-                <p className="font-75">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Ea illo tempora iste amet consequuntur aliquam maiores exercitationem consequatur, 
-                    voluptatem fugit neque architecto modi ex deleniti commodi! Odio provident, 
-                    iusto molestiae dicta commodi quisquam veniam eos accusantium ducimus molestias, 
-                    nostrum voluptates eveniet reiciendis excepturi repellat consequuntur distinctio 
-                    error ipsa laudantium quo vitae, porro fugiat quaerat nesciunt. Ad tenetur nemo velit nam. 
-                    Aperiam neque iste vel soluta dolore, voluptatum optio non doloribus ipsa doloremque 
-                    quibusdam nulla deleniti nemo nisi.
-                </p>
+            <div className="d-flex justify-content-between mt-5 mx-5">
+                <Col>
+                    <h3 className="d-flex solid-bottom-border-secondary">About</h3>
+                    <p className="font-65">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Ea illo tempora iste amet consequuntur aliquam maiores exercitationem consequatur, 
+                        voluptatem fugit neque architecto modi ex deleniti commodi! Odio provident, 
+                        iusto molestiae dicta commodi quisquam veniam eos accusantium ducimus molestias, 
+                        nostrum voluptates eveniet reiciendis excepturi repellat consequuntur distinctio 
+                        error ipsa laudantium quo vitae, porro fugiat quaerat nesciunt. Ad tenetur nemo velit nam. 
+                        Aperiam neque iste vel soluta dolore, voluptatum optio non doloribus ipsa doloremque 
+                        quibusdam nulla deleniti nemo nisi.
+                    </p>
+                </Col>
+                <Col className="map-card m-3">
+                    <Row>
+                        <Image src={map} className="map-img" />
+                    </Row>
+                    <div>
+                        <FaMapMarkerAlt />
+                        <span className="font-60 my-3">Avenida Paulista 2073, São Paulo, São Paulo</span>
+                    </div>
+                </Col>
             </div>
         </div>
     );
