2fa62525e78695b93c6158a58943b1c43b7acf7b
Leandro Costa <leandro.franc2008@hotmail.com>
2021-09-17 17:17:58 -0300

    StudioCard basics

--- frontend/src/components/studios/Studios.jsx
+++ frontend/src/components/studios/Studios.jsx
@@ -1,7 +1,32 @@
 import './Studios.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import Form from 'react-bootstrap/Form';
 
+import StudioCard from './fragments/StudioCard';
+
 const Studios = () => {
+    const [studios, setStudios] = useState([]);
+
+    useEffect(() => {
+        fetchStudios();
+    }, []);
+
+    const fetchStudios = async () => {
+        const res = await axios.get('/api/studios');
+
+        if (res.data) {
+            buildStudios(res.data);
+        } else {
+            console.log(res.error);
+        }
+    }
+
+    const buildStudios = studios => {
+        setStudios(studios);
+    }
+
     return (
         <div className="full-content">
             <div className="search-header">
@@ -15,81 +40,11 @@ const Studios = () => {
             <hr className="my-3" />
 
             <div className="d-flex flex-wrap justify-content-center mx-5">
-                {/* <div className="studio-card m-3">
-                    <Row>
-                        <Image src={img1} className="studio-card-img" />
-                    </Row>
-                    <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
-                        <Col>
-                            <div className="d-flex font-60 my-1">Studio 1</div>
-                            <div className="d-flex">
-                                <FaMapMarkerAlt size={14} />
-                                <span className="font-45 ms-1">Beco Abunã 257, Petrópolis, Manaus - AM</span>
-                            </div>
-                        </Col>
-                    </div>
-                </div>
-                <div className="studio-card m-3">
-                    <Row>
-                        <Image src={img2} className="studio-card-img" />
-                    </Row>
-                    <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
-                        <Col>
-                            <div className="d-flex font-60 my-1">Studio 2</div>
-                            <div className="d-flex">
-                                <FaMapMarkerAlt size={14} />
-                                <span className="font-45 ms-1">Avenida Nepal 755, Nova Cidade, Manaus - AM</span>
-                            </div>
-                        </Col>
-                    </div>
-                </div>
-                <div className="studio-card m-3">
-                    <Row>
-                        <Image src={img3} className="studio-card-img" />
-                    </Row>
-                    <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
-                        <Col>
-                            <div className="d-flex font-60 my-1">Studio 3</div>
-                            <div className="d-flex">
-                                <FaMapMarkerAlt size={14} />
-                                <span className="font-45 ms-1">Rua Mestre Albano 620, Asa Branca, Boa Vista - RR</span>
-                            </div>
-                        </Col>
-                    </div>
-                </div>
-                <div className="studio-card m-3">
-                    <Row>
-                        <Image src={img4} className="studio-card-img" />
-                    </Row>
-                    <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
-                        <Col>
-                            <div className="d-flex font-60 my-1">Studio 4</div>
-                            <div className="d-flex">
-                                <FaMapMarkerAlt size={14} />
-                                <span className="font-45 ms-1">Rua Vinte 511, Praia da Esparança, Magé - RJ</span>
-                            </div>
-                        </Col>
-                    </div>
-                </div>
-                <div className="studio-card m-3">
-                    <Row>
-                        <Image src={img5} className="studio-card-img" />
-                    </Row>
-                    <div className="d-flex align-items-center mb-1">
-                        <Image src={avatar} className="studio-avatar-img" roundedCircle />
-                        <Col>
-                            <div className="d-flex font-60 my-1">Studio 5</div>
-                            <div className="d-flex">
-                                <FaMapMarkerAlt size={14} />
-                                <span className="font-45 ms-1">Rua do Calçadão 212, Duque de Caxias, São Leopoldo - RS</span>
-                            </div>
-                        </Col>
-                    </div>
-                </div> */}
+                {
+                    studios ?
+                    studios.map(studio => <StudioCard studio={studio} />)
+                    : null
+                }
             </div>
         </div>
     );
--- frontend/src/components/studios/fragments/StudioCard.jsx
+++ frontend/src/components/studios/fragments/StudioCard.jsx
@@ -3,26 +3,22 @@ import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import { FaMapMarkerAlt } from 'react-icons/fa';
 
-import img1 from '../../assets/studio/0.jpeg';
-import img2 from '../../assets/studio/3.jpeg';
-import img3 from '../../assets/studio/9.jpeg';
-import img4 from '../../assets/studio/5.jpeg';
-import img5 from '../../assets/studio/1.jpeg';
-import avatar from '../../assets/user_w.png';
+import coverPlaceholder from '../../../assets/cover.jpg';
+import avatarPlaceholder from '../../../assets/user_w.png';
 
-const StudioCard = ({}) => {
+const StudioCard = ({ studio: { coverImg = { publicId: null }, logo = { publicId: null }, name, location } }) => {
     return (
         <div className="studio-card m-3">
             <Row>
-                <Image src={img1} className="studio-card-img" />
+                <Image src={coverImg.publicId ?? coverPlaceholder} className="studio-card-img" />
             </Row>
             <div className="d-flex align-items-center mb-1">
-                <Image src={avatar} className="studio-avatar-img" roundedCircle />
+                <Image src={logo.publicId ?? avatarPlaceholder} className="studio-avatar-img" roundedCircle />
                 <Col>
-                    <div className="d-flex font-60 my-1">Studio 1</div>
+                    <div className="d-flex font-60 my-1">{name}</div>
                     <div className="d-flex">
                         <FaMapMarkerAlt size={14} />
-                        <span className="font-45 ms-1">Beco Abunã 257, Petrópolis, Manaus - AM</span>
+                        <span className="font-45 ms-1">{location.address}</span>
                     </div>
                 </Col>
             </div>
