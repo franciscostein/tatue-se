187f7c3e8768482f79eab42801a31fefd2132675
Leandro Costa <leandro.franc2008@hotmail.com>
2021-09-16 21:24:35 -0300

    ArtistCard placeholders

--- frontend/src/components/artists/fragments/ArtistCard.jsx
+++ frontend/src/components/artists/fragments/ArtistCard.jsx
@@ -2,17 +2,20 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 
-const ArtistCard = ({ artist, artist: { coverPicture, profilePicture, fullName, workplaces, tattooStyles } }) => {
+import coverPlaceholder from '../../../assets/cover.jpg';
+import avatarPlaceholder from '../../../assets/user_w.png';
+
+const ArtistCard = ({ artist: { coverPicture = { publicId: null }, profilePicture = { publicId: null }, fullName, workplaces, tattooStyles } }) => {
     return (
         <div className="studio-card m-3">
             <Row>
-                <Image src={coverPicture.publicId} className="studio-card-img" />
+                <Image src={coverPicture.publicId ?? coverPlaceholder} className="studio-card-img" />
             </Row>
             <div className="d-flex align-items-center mb-1">
-                <Image src={profilePicture.publicId} className="studio-avatar-img" roundedCircle />
+                <Image src={profilePicture.publicId ?? avatarPlaceholder} className="studio-avatar-img" roundedCircle />
                 <Col>
                     <Row className="font-60 ps-3">{fullName}</Row>
-                    <Row className="font-45 ps-3">{workplaces[0].name}</Row>
+                    <Row className="font-45 ps-3">{workplaces.length > 0 ? workplaces[0].name : null}</Row>
                 </Col>
             </div>
             {
