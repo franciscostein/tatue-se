8e5e1966820fa24637db52ccd46cf39d178772f8
Leandro Costa <leandro.franc2008@hotmail.com>
2021-11-27 17:03:12 -0300

    general SignUp form validation

--- frontend/src/components/signup/SignUp.jsx
+++ frontend/src/components/signup/SignUp.jsx
@@ -3,41 +3,49 @@ import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import { FaExclamationTriangle } from 'react-icons/fa';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
-    const [emailError, setEmailError] = useState('');
     const [password, setPassword] = useState('');
+    const [isPasswordInvalid, setIsPasswordInvalid] = useState(false);
     const [passwordConfirmation, setPasswordConfirmation] = useState('');
-    const [passwordsMatch, setPasswordsMatch] = useState(true);
-    const [validated, setValidated] = useState(false);
+    const [ispasswordConfirmationInvalid, setIsPasswordConfirmationInvalid] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
     const handleSubmit = event => {
-        const form = event.currentTarget;
-
-        // isPasswordMatched();
-
-        if (!form.checkValidity()) {
+        if (!validate()) {
             event.preventDefault();
             event.stopPropagation();
         }
-
-        setValidated(true);
     }
 
-    // const isPasswordMatched = () => {
-    //     if (password !== passwordConfirmation) {
-    //         setErrorMessage('Passwords do not match');
-    //     }
-    // }
+    const validate = () => {
+        if (password !== passwordConfirmation) {
+            setIsPasswordInvalid(true);
+            setIsPasswordConfirmationInvalid(true);
+            setErrorMessage(`Passwords don't match.`);
+            return false;
+        }
+        return true;
+    }
 
     return (
         <Container>
             <div className="d-flex solid-bottom-border-secondary mb-5">
                 <h1>SignUp</h1>
             </div>
-            <Form noValidate validated={validated} onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit}>
+                {
+                    errorMessage &&
+                    (
+                        <Alert variant="danger" className="mb-4 mx-3">
+                            <FaExclamationTriangle className="me-3" />
+                            <span className="font-80">{errorMessage}</span>
+                        </Alert>
+                    )
+                }
                 <Form.Group controlId="formSignUpEmail" className="mb-3 mx-5">
                     <Form.Label className="font-75">E-mail</Form.Label>
                     <Form.Control 
@@ -51,18 +59,22 @@ const SignUp = () => {
                     <Form.Label className="font-75">Password</Form.Label>
                     <Form.Control 
                         required
+                        minLength={7}
                         type="password"
                         value={password}
                         onChange={e => setPassword(e.target.value)}
+                        isInvalid={isPasswordInvalid}
                     />
                 </Form.Group>
                 <Form.Group controlId="formSignUpPassword" className="mb-3 mx-5">
                     <Form.Label className="font-75">Confirm password</Form.Label>
                     <Form.Control 
                         required
+                        minLength={7}
                         type="password"
                         value={passwordConfirmation}
                         onChange={e => setPasswordConfirmation(e.target.value)}
+                        isInvalid={ispasswordConfirmationInvalid}
                     />
                 </Form.Group>
                 <Button variant="primary" type="submit" size="lg" className="mt-4">
