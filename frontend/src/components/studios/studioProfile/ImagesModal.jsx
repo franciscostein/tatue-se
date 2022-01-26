9c124cddff0425e98788e4c6a24f2c89c6ffa014
Leandro Costa <leandro.franc2008@hotmail.com>
2022-01-25 22:40:23 -0300

    ImagesModal layout improved

--- frontend/src/components/studios/studioProfile/ImagesModal.jsx
+++ frontend/src/components/studios/studioProfile/ImagesModal.jsx
@@ -9,11 +9,11 @@ const ImagesModal = ({ show, closeFunction, cover, photos }) => {
                 <Modal.Title>Add photos</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Image src={cover} style={{ width: '100%', height: '27vh', objectFit: 'cover', borderRadius: '16px' }} />
+                <Image src={cover} style={{ width: '100%', height: '27vh', objectFit: 'cover', borderRadius: '16px' }} className="clickable" />
                 <hr />
                 <div className="d-flex flex-wrap justify-content-center my-3">
                     {
-                        photos && photos.map(photo => <Image key={photo._id} src={photo.publicId} className="m-2 studio-img" />)
+                        photos && photos.map(photo => <Image key={photo._id} src={photo.publicId} className="m-2 studio-img studio-img-modal clickable" />)
                     }
                 </div>
             </Modal.Body>
