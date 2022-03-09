2a9040c286570ee1dcd644c61039810a6168585b
Leandro Costa <leandro.franc2008@hotmail.com>
2022-03-09 20:21:07 -0300

    upgraded express limit to 50mb

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,7 @@ const app = express();
 connectDB();
 
 app.use(cors());
-app.use(express.json({ limit: '25mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/auth', auth);
 app.use('/api/users', users);
