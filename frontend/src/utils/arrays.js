f858750aaab5d5bd15f6b220231cea2b5f5fc337
Leandro Costa <leandro.franc2008@hotmail.com>
2022-03-25 09:55:41 -0300

    splitted files

--- frontend/src/utils/arrays.js
+++
@@ -1,6 +0,0 @@
-export const isEmpty = array => array.length === 0;
-
-export const isNotEmpty = array => array.length > 0;
-
-export const intersect = (arrayA, arrayB) =>
-	arrayA.filter(a => arrayB.some(b => b._id === a._id));
